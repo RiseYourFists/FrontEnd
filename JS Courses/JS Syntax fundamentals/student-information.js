379bdc4f32386f9fc2f5a99f14ba430bef024b8f
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-05 19:47:40 +0300

    Problem solving

---
+++ JS Courses/JS Syntax fundamentals/student-information.js
@@ -0,0 +1,7 @@
+function printsStudentInfo(name, age, avgGrade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${avgGrade.toFixed(2)}`)
+}
+
+printsStudentInfo('John', 15, 5.54678)
+printsStudentInfo('Steve', 16, 2.1426)
+printsStudentInfo('Marry', 12, 6.00)
