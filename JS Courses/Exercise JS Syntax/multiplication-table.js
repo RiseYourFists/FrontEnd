c9df25c082fb3c31767577b92a66b5e2e7f50148
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-08 19:32:30 +0300

    Problem solving

---
+++ JS Courses/Exercise JS Syntax/multiplication-table.js
@@ -0,0 +1,8 @@
+function multiplicationTable(multiplier){
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${multiplier} X ${i} = ${multiplier * i}`);
+        
+    }
+}
+
+multiplicationTable(5);
