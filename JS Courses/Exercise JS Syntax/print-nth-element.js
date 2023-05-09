4be9b25da877277082fc238e5ba59b3d28aa997e
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-09 13:20:30 +0300

    Problem solving

---
+++ JS Courses/Exercise JS Syntax/print-nth-element.js
@@ -0,0 +1,11 @@
+function printNth(arr, step){
+    let result = [];
+    for(i = 0; i < arr.length; i+=step){
+        result.push(arr[i]);
+    }
+    return result;
+}
+
+console.log(printNth(['5','20', '31', '4', '20'], 2)); 
+console.log(printNth(['dsa','asd','test','tset'], 2));
+console.log(printNth(['1', '2', '3', '4', '5'], 6));
