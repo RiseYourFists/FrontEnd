1d18e0cfaad7d16000fe4e060dbf2245f96d7836
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-21 00:29:50 +0300

    Problem solving

--- JS Courses/DOM and Events/01. Sum-Numbers/SumNumbers.js
+++ JS Courses/DOM and Events/01. Sum-Numbers/SumNumbers.js
@@ -1,3 +1,10 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+    let result = document.getElementById('sum');
+    
+    let first = Number(num1.value);
+    let second = Number(num2.value);
+
+    result.value = first + second;
 }
