68fd18e9a4c02b7ee76d71f93fcf10be489ca70b
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-21 00:57:08 +0300

    Problem solving

--- JS Courses/DOM and Events/03. Collect-List-Items/CollectListItems.js
+++ JS Courses/DOM and Events/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,9 @@
 function extractText() {
-    // TODO
+    const list = document.getElementsByTagName('li');
+    let text = [];
+    for (const element of list) {
+        text.push(element.textContent)
+    }
+    const result = document.getElementById('result');
+    result.textContent = text.join('\n')
 }
