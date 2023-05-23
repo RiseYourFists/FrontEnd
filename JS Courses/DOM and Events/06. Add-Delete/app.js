e6e6b5cd917cac5fdaa2b4d0c12c2307e8ea5d65
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-23 16:13:54 +0300

    Problem solving

--- JS Courses/DOM and Events/06. Add-Delete/app.js
+++ JS Courses/DOM and Events/06. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    let input = document.getElementById('newItemText');
+    let items = document.getElementById('items');
+    let newLiItem = document.createElement('li');
+
+    let deleteBtn = document.createElement('a')
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.setAttribute('href', '#');
+    deleteBtn.addEventListener('click', delElement);
+
+    newLiItem.textContent = input.value;
+    newLiItem.appendChild(deleteBtn);
+
+    items.appendChild(newLiItem);
+    
+
+    function delElement(e){
+        let target = e.target;
+        target.parentElement.remove();
+    }
 }
