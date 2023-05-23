8518c55ae6d0ef75c724c7fc840390671dd46cbd
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-23 16:31:52 +0300

    Problem solving

--- JS Courses/DOM and Events/07. Colorized-Table/ColorizeTable.js
+++ JS Courses/DOM and Events/07. Colorized-Table/ColorizeTable.js
@@ -1,3 +1,7 @@
 function colorize() {
-    // TODO
+    const tableRows = Array.from(document.querySelectorAll('table tr:nth-child(even)'));
+    
+    for (const tr of tableRows) {
+       tr.style.backgroundColor = 'teal';
+    }
 }
