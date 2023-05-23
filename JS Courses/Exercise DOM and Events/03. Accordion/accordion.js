2f7f1d82d658d1ec2ed0f3204613f5c245b9fb08
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-23 20:48:37 +0300

    Problem solving

--- JS Courses/Exercise DOM and Events/03. Accordion/accordion.js
+++ JS Courses/Exercise DOM and Events/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    console.log('TODO:...');
+    const toggleBtn = document.querySelector('span.button');
+    const extraWindow = document.getElementById('extra');
+
+    let state = toggleBtn.textContent;
+
+    if(state === 'More'){
+        extraWindow.style.display = 'block';
+        toggleBtn.textContent = 'Less';
+        return;
+    }
+    
+    extraWindow.style.display = 'none';
+    toggleBtn.textContent = 'More';
 }
