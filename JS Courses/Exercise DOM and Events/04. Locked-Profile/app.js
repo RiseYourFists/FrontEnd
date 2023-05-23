adc00e354cf4a4bf2bd61a72d62235f6fd0ff960
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-23 23:51:34 +0300

    Problem solving

--- JS Courses/Exercise DOM and Events/04. Locked-Profile/app.js
+++ JS Courses/Exercise DOM and Events/04. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const profiles = document.querySelectorAll('div.profile');
+    for (const profile of profiles) {
+        let button = profile.children[10];
+        button.addEventListener('click', onClickHandler);
+    }
+    
+    function onClickHandler(e){
+        const button = e.target;
+        const profile = button.parentElement;
+        const extraField = profile.children[9];
+        
+        if(profile.children[2].checked){
+            return;
+        }
+
+        if(button.textContent === 'Show more'){
+            extraField.style.display = 'block';
+            button.textContent = 'Hide it';
+            return;
+        }
+
+        extraField.style.display = 'none';
+        button.textContent = 'Show more';
+
+    }
 }
