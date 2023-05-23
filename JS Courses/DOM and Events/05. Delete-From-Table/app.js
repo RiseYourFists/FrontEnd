0a976b57d6d8d4a123d4acbef071381f1062bbd1
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-23 15:45:26 +0300

    Problem solving

--- JS Courses/DOM and Events/05. Delete-From-Table/app.js
+++ JS Courses/DOM and Events/05. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
-}
+    let input = document.querySelector('input[name="email"]');
+    let result = document.getElementById('result');
+    let emailTarget = input.value;
+    let emails = Array.from(document.querySelectorAll('#customers > tbody > tr > td:nth-child(even)'));
+    let emailToDelete = emails.find((email)=> email.textContent === emailTarget);
+
+    if(emailToDelete === undefined){
+        result.textContent = 'Not found.'
+    }
+    else{
+        emailToDelete.parentElement.remove();
+        result.textContent = 'Deleted.'
+    }
+
+    input.value = '';
+}   
