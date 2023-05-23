fd5100761065a7293c078a5bc70bf4f3a2b0307c
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-24 00:35:34 +0300

    Problem solving

--- JS Courses/Exercise DOM and Events/06. Table - Search Engine/solution.js
+++ JS Courses/Exercise DOM and Events/06. Table - Search Engine/solution.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const targetInput = document.getElementById('searchField');
+      const tableElements = Array.from(document.querySelectorAll('tbody > tr > td'));
+      const rows = Array.from(document.querySelectorAll('tbody > tr'));
+      for (const row of rows) {
+         row.removeAttribute('class','select');
+      }
 
+      for (const definition of tableElements) {
+         if(definition.textContent.includes(targetInput.value)){
+            definition.parentElement.setAttribute('class', 'select');
+         }
+      }
+
+      targetInput.value = '';
    }
 }
