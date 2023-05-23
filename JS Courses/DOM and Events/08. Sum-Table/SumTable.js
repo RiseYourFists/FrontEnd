bf4a2f8f23833f3765bb244d9e962d37f8b60b67
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-23 16:41:03 +0300

    Problem solving

--- JS Courses/DOM and Events/08. Sum-Table/SumTable.js
+++ JS Courses/DOM and Events/08. Sum-Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    let values = document.querySelectorAll('tbody tr > td:nth-child(even)')
+    let result = document.getElementById('sum');
+    let sum = 0;
 
+    for (const number of values) {
+        sum+= Number(number.textContent);
+    }
+
+    result.textContent = sum;
 }
