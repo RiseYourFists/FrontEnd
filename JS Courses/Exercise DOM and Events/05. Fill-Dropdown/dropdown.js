182f090533710992636b3d35d5a90460da93bac7
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-24 00:05:40 +0300

    Problem solving

--- JS Courses/Exercise DOM and Events/05. Fill-Dropdown/dropdown.js
+++ JS Courses/Exercise DOM and Events/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const itemTextInput = document.getElementById('newItemText');
+    const valueTextInput = document.getElementById('newItemValue');
+    const optionMenu = document.getElementById('menu');
+
+    let newElement = document.createElement('option');
+    newElement.setAttribute('value', valueTextInput.value);
+    newElement.textContent = itemTextInput.value;
+
+    optionMenu.appendChild(newElement);
+    itemTextInput.value = '';
+    valueTextInput.value = '';
 }
