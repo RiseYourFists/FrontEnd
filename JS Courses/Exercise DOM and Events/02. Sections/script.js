912324ec4ad0302990f13feddb5684cd2bb51be2
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-23 20:29:28 +0300

    Problem solving

--- JS Courses/Exercise DOM and Events/02. Sections/script.js
+++ JS Courses/Exercise DOM and Events/02. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+   const wrapperDiv = document.getElementById('content');
+
+   words.forEach(element => {
+      let paragraph = document.createElement('p')
+      paragraph.textContent = element;
+      paragraph.style.display = 'none';
+
+      let wrapper = document.createElement('div');
+      wrapper.appendChild(paragraph);
+      wrapper.addEventListener('click', onClickHandler);
+      
+      wrapperDiv.appendChild(wrapper);
+   });
+
+   function onClickHandler(e){
+      let target = e.target;
+      target.children[0].style.display = 'block';
+   }
 }
