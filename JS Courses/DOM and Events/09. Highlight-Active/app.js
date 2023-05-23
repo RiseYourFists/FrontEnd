137e27c751453e75d0feb2e43654623a1b2faa6d
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-23 17:01:35 +0300

    Problem solving

--- JS Courses/DOM and Events/09. Highlight-Active/app.js
+++ JS Courses/DOM and Events/09. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+    const elements = Array.from(document.querySelectorAll('input[type="text"]'));
+    
+    for (const element of elements) {
+        element.addEventListener('focus', onFocusHandler)
+        element.addEventListener('blur', onBlurHandler)
+    }
+
+    function onFocusHandler(e){
+        const target = e.target;
+        let parent = target.parentElement;
+        parent.setAttribute('class','focused');
+    }
+
+    function onBlurHandler(e){
+        const target = e.target;
+        let parent = target.parentElement;
+        parent.removeAttribute('class', 'focused')
+    }
 }
