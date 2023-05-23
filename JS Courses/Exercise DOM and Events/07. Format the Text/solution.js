07e820fbe0aacbd88efef3f7117aeddbf756969d
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-24 01:04:09 +0300

    Problem solving

--- JS Courses/Exercise DOM and Events/07. Format the Text/solution.js
+++ JS Courses/Exercise DOM and Events/07. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  const inputElement = document.getElementById('input');
+  const outputElement = document.getElementById('output');
+  let text = inputElement.value;
+  let sentences = text.split('.');
+  sentences.pop();
+
+  while(sentences.length > 0){
+    let paragraphSentences = sentences
+      .splice(0, 3)
+      .map((p)=> p.trimStart());
+
+    const newParagraph = document.createElement('p');
+
+    newParagraph.textContent = paragraphSentences.join('.') + '.';
+    outputElement.appendChild(newParagraph);
+  }
 }
