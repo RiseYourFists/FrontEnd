2318df1b6ee39c7f35fab41e3e8e561cf3a9e056
Emanuel Shavepeyer <daru12378@gmail.com>
2023-05-31 16:05:06 +0300

    Problem solving

--- JS Courses/Exercise Http and REST/02.Bus-Schedule/app.js
+++ JS Courses/Exercise Http and REST/02.Bus-Schedule/app.js
@@ -1,10 +1,49 @@
 function solve() {
+    const departInput = document.getElementById('depart');
+    const arriveInput = document.getElementById('arrive');
+    const display = document.querySelector('.info');
+
+    const BASE_URL = "http://localhost:3030/jsonstore/bus/schedule/";
+    let isMoving = false;
+    let currentStop = 'depot';
+    let stopName = '';
+    let nextStop = '';
+    
     function depart() {
-        // TODO: 
+
+        isMoving = true;
+        movingStateHandler();
+
+        fetch(BASE_URL + currentStop)
+        .then((res)=> res.json())
+        .then((data)=>{
+            stopName = data.name;
+            nextStop = data.next;
+            display.textContent = `Next stop ${stopName}`;
+        });
+        
     }
 
     async function arrive() {
-        // TODO:
+
+        isMoving = false;
+        movingStateHandler();
+
+        currentStop = nextStop;
+
+        display.textContent = `Arriving at ${stopName}`;
+    }
+
+    function movingStateHandler(){
+        
+        if(isMoving){
+            arriveInput.disabled = false;
+            departInput.disabled = true;
+            return;
+        }
+
+        arriveInput.disabled = true;
+        departInput.disabled = false;
     }
 
     return {
